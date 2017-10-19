969a4dd0fd56b75bea7b6c8907fb91177f96b478
AnazzKa <anaska582@gmail.com>
2017-10-19 00:38:36 -0700

    fv

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,3 +4,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'main';
 $route['login'] = 'login/index';
 $route['login_action']='login/login_action';
+$route['Dashboard']='home/index';
